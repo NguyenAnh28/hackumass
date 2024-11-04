e879099a6655f5ae022cfd8d430c6f606fcec32c
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2024-11-04 12:01:48 -0500

    POST

--- test/test.py
+++ test/test.py
@@ -3,6 +3,11 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Student(BaseModel):
+    name: str
+    age: int
+    major: str
+
 @app.get("/")
 async def read_root():
     return{"message": "Welcome to the Umass Web App API"}
@@ -14,3 +19,8 @@ async def get_courses():
 @app.get("/students/{student_id}")
 async def get_student(student_id: int):
     return {"student_id": student_id, "name": "John Doe", "major": "Computer Science"}
+
+
+@app.post("/students/")
+async def create_student(student: Student):
+    return {"message": "Student created", "student": student}
